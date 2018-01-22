1b6c1c37302245b283f05c858400a6794859dcdb
Sergey Cheperis <czz@czz.name>
2018-01-22 18:10:10 +0300

    Change focus rectangle look

--- ios/RCT/CameraFocusSquare.m
+++ ios/RCT/CameraFocusSquare.m
@@ -11,14 +11,14 @@ const float squareLength = 80.0f;
         // Initialization code
 
         [self setBackgroundColor:[UIColor clearColor]];
-        [self.layer setBorderWidth:2.0];
+        [self.layer setBorderWidth:1.0];
         [self.layer setCornerRadius:4.0];
-        [self.layer setBorderColor:[UIColor whiteColor].CGColor];
+        [self.layer setBorderColor:[UIColor colorWithRed:1 green:1 blue:1 alpha:0.5].CGColor];
 
         CABasicAnimation* selectionAnimation = [CABasicAnimation
                                                 animationWithKeyPath:@"borderColor"];
-        selectionAnimation.toValue = (id)[UIColor blueColor].CGColor;
-        selectionAnimation.repeatCount = 8;
+        selectionAnimation.toValue = (id)[UIColor colorWithRed:1 green:1 blue:0 alpha:0.5].CGColor;
+        selectionAnimation.repeatCount = 4;
         [self.layer addAnimation:selectionAnimation
                           forKey:@"selectionAnimation"];
 
